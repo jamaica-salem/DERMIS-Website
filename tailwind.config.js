318efa51944f24e8579fdeb70a3aa5857b405a51
Jamaica Salem <jamaica.esalem@gmail.com>
2024-05-14 22:52:43 +0800

    Edit navigation bar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3C5E39',
+        secondary: '#83AEA2',
+        customGreen: '#3D7868',
+        customWhite: '#F2F6EF',
+      },
+    },
+  },
+  plugins: [],
+}
+
